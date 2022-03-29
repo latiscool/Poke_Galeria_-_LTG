34baa2f516a62567c261493e99852d6853595a28
latiscool <luistorresgomez.cl@gmail.com>
2022-03-28 21:51:24 -0300

    Update poke.js

--- poke.js
+++ poke.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const open = require('open');
 const PORT = 3000;
 const host = 'localhost';
-const urlBase = 'https://pokeapi.co/api/v2/pokemon?limit=20;
+const urlBase = 'https://pokeapi.co/api/v2/pokemon?limit=20';
 let lista = [];
 
 open(`http://${host}:${PORT}/`, function (err) {
