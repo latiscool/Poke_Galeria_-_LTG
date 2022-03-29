a2a7473962588d0b25b38ea0bc9d9d32e04748eb
Luis Torre Gomez <luistorresgomez.cl@gmail.com>
2022-03-28 21:29:13 -0300

    Desafio Galeria Pokemones - LTG

---
+++ README.md
@@ -0,0 +1,12 @@
+  <h1>GALERIA POKEMONES</h1>
+  <h3>Llamdas Asincronas</h3>
+  <ol>
+    <li>Uso de Asyn/Await</li>
+    <li>Axios</li>
+    <li>Promise</li>
+    <li>NodeJs</li>
+    <li>ECMAScript 6 - Funcion Flecha</li>
+    <li>IIFE</li>
+    <li>Extra: Dependencia Open</li>
+
+  </ol>
---
+++ poke.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const open = require('open');
+const PORT = 3000;
+const host = 'localhost';
+const urlBase = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+let lista = [];
+
+open(`http://${host}:${PORT}/`, function (err) {
+  if (err) throw err;
+});
+
+const requestListener = (req, res) => {
+  //BLOQUE HTML
+  if (req.url == '/') {
+    res.writeHead(200, { 'Contente-Type': 'text/html' });
+    fs.readFile('index.html', 'utf-8', (err, html) => {
+      if (err) {
+        console.log('Ha ocurrida una Falla : ' + err);
+      }
+      res.end(html);
+    });
+  }
+  //BLOQUE POKE
+  if (req.url.startsWith('/pokemones')) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    (async () => {
+      await axios.get(urlBase).then((poke) => {
+        let array = poke.data.results;
+        array.map((p) => {
+          getFullData(p.name).then((resultado) => {
+            lista.push({
+              nombre: `${resultado.name}`,
+              img: `${resultado.sprites.front_default}`,
+            });
+          });
+        });
+        Promise.all(lista).then((data) => {
+          res.end(JSON.stringify(data));
+        });
+      });
+    })();
+
+    ///Pasando al getFullData el ARGUMENTO nombre CON pokeName(), linea 27
+    const getFullData = async (name) => {
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`
+      );
+      return data;
+    };
+  }
+};
+//BLOQUE LEVANTANDO SERVER
+const server = http.createServer(requestListener);
+server.listen(PORT, host, () => {
+  console.log(`El servidor se esta ejecutando en http://${host}:${PORT}`);
+});
